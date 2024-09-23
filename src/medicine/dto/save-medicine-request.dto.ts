8b55d5c402774c1a63ae6749b8746df270075eae
wheon06 <up2kid@gmail.com>
2024-09-23 13:07:26 +0900

    🚩 Swagger 업데이트

--- src/medicine/dto/save-medicine-request.dto.ts
+++ src/medicine/dto/save-medicine-request.dto.ts
@@ -1,10 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SaveMedicineRequestDto {
+  @ApiProperty()
   breakfast: string;
+  @ApiProperty()
   checkedBreakfast: boolean;
+  @ApiProperty()
   lunch: string;
+  @ApiProperty()
   checkedLunch: boolean;
+  @ApiProperty()
   dinner: string;
+  @ApiProperty()
   checkedDinner: boolean;
+  @ApiProperty()
   date: string;
+  @ApiProperty()
   userId: number;
 }
--- src/medicine/medicine.controller.ts
+++ src/medicine/medicine.controller.ts
@@ -6,21 +6,54 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { MedicineService } from './medicine.service';
 import { SaveMedicineRequestDto } from './dto/save-medicine-request.dto';
+import {
+  ApiCreatedResponse,
+  ApiHeader,
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { Medicine } from './entities/medicine.entity';
 
 @Controller('medicine')
 export class MedicineController {
   constructor(private readonly medicineService: MedicineService) {}
 
+  @ApiTags('약 정보 API')
+  @ApiOperation({
+    summary: '약 정보 저장 API',
+  })
+  @ApiCreatedResponse({ type: Medicine })
+  @ApiHeader({ name: 'Authorization', description: 'Bearer [accessToken]' })
+  @UseGuards(AuthGuard())
   @Post('save')
   async save(@Body() reqDto: SaveMedicineRequestDto) {
     return await this.medicineService.save(reqDto);
   }
 
-  @Patch(':userId')
-  async updateIsChecked(@Param('userId') userId: number, @Query() query: any) {
+  @ApiTags('약 정보 API')
+  @ApiOperation({
+    summary: '약 체크 유무 업데이트 API',
+  })
+  @ApiOkResponse({ type: [Number] })
+  @ApiHeader({ name: 'Authorization', description: 'Bearer [accessToken]' })
+  @ApiQuery({ name: 'index', required: true, type: String })
+  @ApiQuery({ name: 'value', required: true, type: String })
+  @ApiQuery({
+    name: 'date',
+    required: true,
+    description: '형식 : 2024-01-01',
+    type: String,
+  })
+  @UseGuards(AuthGuard())
+  @Patch(':id')
+  async updateIsChecked(@Param('id') userId: number, @Query() query: any) {
     return await this.medicineService.updateIsChecked(
       userId,
       query.index,
@@ -29,6 +62,19 @@ export class MedicineController {
     );
   }
 
+  @ApiTags('약 정보 API')
+  @ApiOperation({
+    summary: '약 정보 불러오기 API',
+  })
+  @ApiOkResponse({ type: Medicine })
+  @ApiHeader({ name: 'Authorization', description: 'Bearer [accessToken]' })
+  @ApiQuery({
+    name: 'date',
+    required: true,
+    description: '형식 : 2024-01-01',
+    type: String,
+  })
+  @UseGuards(AuthGuard())
   @Get(':id')
   async getMedicine(@Param('id') id: number, @Query() query: any) {
     return await this.medicineService.findByDate(id, query.date);
